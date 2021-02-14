7b78552b739256371e29fe5174ff1bf149a57f1b
sakiratsui <36963444+sakiratsui@users.noreply.github.com>
2021-02-14 16:22:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def logon():
                 #usr.username = name
                 #usr.usertype = row[3]
                 #flask_login.login_user(usr)
-                return render_template("exams.html");
+                return render_template("exams.html", user_type= "Ogretmen", exam=createdexams);
             else:
                 return "<script> alert('Wrong username or password!'); </script>" + render_template("home.html")
     # bu değerler db'de bir veriyle eşleşirse home'a gidilir.
