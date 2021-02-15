fb0381f58ac0b3ce0d044ecc34ba2f0caf4081b1
sakiratsui <36963444+sakiratsui@users.noreply.github.com>
2021-02-15 21:20:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 import sys
-
+from templates.exam import Exam
+from templates.question import Question
 import flask_login
 import uuid
 from flask import Flask, request, render_template, redirect, url_for, jsonify, json
 from flask_login import login_required
 from psycopg2._psycopg import cursor
-
+import manage
 from database import Database
-
-
 app = Flask(__name__)
 
 
-#users = {'sakiratsui': {'password': 'secret'}, 'dogukan': {'password': '1234'}}
-
 exams = []
-createdexams = []  # tıklandıktan sonra kaydedilmeleri için
-
-
+createdexams = manage.getExamFromDataBase() # tıklandıktan sonra kaydedilmeleri için
 # db'den çekilecek
-
 class User(flask_login.UserMixin):
     def __init__(self, username, password, usertype):
         self.username = username
@@ -27,49 +21,49 @@ class User(flask_login.UserMixin):
         self.usertype = usertype
 
 
-
 @app.route("/")
 def redirecthome():
     return redirect(url_for("login"))
 
-
-@app.route("/home")
+@app.route("/home", methods=["POST","GET"])
 def login():
-    return render_template('home.html')  # userexists=current_user , user varsa tekrar login kısmını göstermesin!.
-
-
-@app.route("/home", methods=["POST"])
-def logon():
-    name = request.form.get("name")
-    db = Database()
-    with db.get_cursor() as cursor:
-        cursor.execute("SELECT * FROM Kullanici WHERE kullanici_adi= %s", (name,))
-        rows = cursor.fetchall()
-        for row in rows:
-            if request.form.get("password") == str(row[2]):
-                #usrnm = name
-                usertype = str(row[3])
-                return render_template("exams.html", user_type=usertype, exam=createdexams)
-            else:
-                return "<script> alert('Wrong username or password!'); </script>" + render_template("home.html")
-    # bu değerler db'de bir veriyle eşleşirse home'a gidilir.
-    # else return login again?
+    if request.method == "POST":
+        name = request.form.get("name")
+        db = Database()
+        with db.get_cursor() as cursor:
+            cursor.execute("SELECT * FROM Kullanici WHERE kullanici_adi= %s", (name,))
+            rows = cursor.fetchall()
+            for row in rows:
+                if request.form.get("password") == str(row[2]):
+                    usertype = str(row[3])
+                    return render_template("exams.html", user_type=usertype, exam=createdexams)
+                else:
+                    return "<script> alert('Wrong username or password!'); </script>" + render_template("home.html")
+    return render_template('home.html')
 
 
 @app.route("/exams", methods=["GET", "POST"])
 #@login_required
 def show_exams():
     if request.method == "POST":
-        examdetails = json.loads(request.data)
-        # created exams ve exam details parse edilip eklenecek
-        createdexams.append(exams[-1])
+        exam_number=Exam.exam_count+1
+        exam_object=Exam(exam_number,exams[-1][0], exams[-1][1], exams[-1][2])
+        createdexams.append(exam_object)
+        manage.insertExamDataBase(exam_object)
         print(createdexams, sys.stdout.flush())
-    # Sınav(sınav_id,sinav_adi,sınav_baslama,sınav_bitis)
-        db = Database()
-        with db.get_cursor() as cursor:
-            cursor.execute("INSERT INTO Sinav(sinav_adi,sinav_baslama_tarihi,sinav_bitis_tarihi) VALUES (%s, %s,%s);",
-                       (exams[-1][0], exams[-1][1], exams[-1][2]))
-        db.commit()
+        examdetails = json.loads(request.data)
+        exam_id=manage.getExam(exam_object.exam_name)
+        for i in examdetails["data"]:
+            question=i["value"]["question"]
+            a_choice=i["value"]["a_choice"]
+            b_choice=i["value"]["b_choice"]
+            c_choice=i["value"]["c_choice"]
+            d_choice=i["value"]["d_choice"]
+            e_choice=i["value"]["e_choice"]
+            true_answer_choice=i["value"]["true_answer_choice"]
+            question_point=int(i["value"]["question_point"])
+            all_choice=a_choice+"*_*"+b_choice+"*_*"+c_choice+"*_*"+d_choice+"*_*"+e_choice
+            manage.insertQuestionDataBase(exam_id,question,all_choice,true_answer_choice,question_point)
     return render_template("exams.html", user_type="Ogretmen", exam=createdexams)
 
 
@@ -78,6 +72,22 @@ def show_exams():
 def create_exam():
     return render_template("createexam.html")
 
+@app.route("/exam/<exam_id>")
+def nolr(exam_id):
+    sorular=manage.getQuestion(exam_id)
+    soru_icerik=[]
+    soru_siklari=[]
+    soru_puani=[]
+    for soru in sorular:
+        soru_icerik.append(soru[2])
+        soru_siklari.append(soru[3].split("*_*"))
+        soru_puani.append(soru[5])
+    return render_template("showquestion.html",
+                            exam_id=exam_id,
+                            soru_icerik=soru_icerik,
+                            #soru_siklari=soru_siklari,
+                            #soru_puani=soru_puani
+                          )
 
 @app.route("/createexam/p=2")
 #@login_required
@@ -90,23 +100,18 @@ def exampagetwo():
 
 
 @app.route("/leaderboard")
-@login_required
+#@login_required
 def leaderboard():
-    names = "dogukan", "muge"  # db.get öğrenci sınav tablosundaki kullanıcı adları
-    points = 90, 95  # db.get öğrencisınav tablosundaki notlar
-    point_info = zip(names, points)  # ikili tuple'lar haline getirdi.
-    point_info = sorted(point_info, key=lambda tup: (-tup[1]))  # tuple'ı notlara göre yüksekten düşüğe sıralama
-    return render_template("leaderboard.html", point=point_info)
+    leaderboard_info = manage.getLeaderboardFromDatabase()
+    sorted(leaderboard_info, key=lambda x: x[5], reverse=True)
+    return render_template("leaderboard.html", point=leaderboard_info)
 
 
 @app.route("/logout")
 #@login_required
 def logout():
-    #flask_login.logout_user()
     return redirect(url_for("login"))
-
 
 if __name__ == '__main__':
     app.secret_key = 'j1i5ek0eeg+lb0uj^rvm)d1a@qvz^l&1(ep8f54n(oe+uc6s)4'
-
     app.run(debug=True)
